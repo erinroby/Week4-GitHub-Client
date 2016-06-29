5ebb300f910ea0ccab9a4c01fc3577dc1e82747d
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-06-29 16:42:48 -0700

    finish lab and code challenge

--- Week4CodeChallenges.playground/Contents.swift
+++ Week4CodeChallenges.playground/Contents.swift
@@ -25,18 +25,13 @@ func tupleHighLow(input: [Int]) -> (min: Int?, max: Int?) {
 
 tupleHighLow(test)
 
-<<<<<<< HEAD
 // CLASS 3: Given a non-negative number "num", return true if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2.
 
 func multiple(num: Int) -> Bool {
     return (num + 2) % 10 <= 4
 }
 
+multiple(54)
 
-
-// multiple(54)
-
-=======
->>>>>>> 781010de0681a2a107aa14b154bc92b3f9d1b877
 
 
