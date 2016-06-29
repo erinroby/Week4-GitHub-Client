44391a892ee2140628dafb4c5fcb60c130885ee7
Erin Roby <erinroby@Erins-MacBook-Pro.local>
2016-06-29 10:19:38 -0700

    test

---
+++ GoGoGitHub/GoGoGitHub/Credentials.swift
@@ -0,0 +1,12 @@
+//
+//  Credentials.swift
+//  GoGoGitHub
+//
+//  Created by Erin Roby on 6/27/16.
+//  Copyright © 2016 Erin Roby. All rights reserved.
+//
+
+import Foundation
+
+let kGitHubClientID = "8a32b2b944d000f1250e"
+let kGitHubClientSecret = "8185c9410ed0573f76a2d39fee6088fdfdebe6ef"
